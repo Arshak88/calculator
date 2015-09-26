fc8c65dbfcfdbec0526da1355824c6fc4dcbfadf
Arshak Tumanyan <tumanyan88@gmail.com>
2015-09-26 16:29:25 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <form action="" method="post">
+      <textarea name="calc_window" class="calc_window" disabled></textarea>
+    </form>
+  </body>
+</html>
