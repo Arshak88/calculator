0d772c83340fedff1caa8ba8ea3d1ad80b0b5d10
Arshak Tumanyan <tumanyan88@gmail.com>
2015-09-26 16:46:28 +0400

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
   <body>
     <form action="" method="post">
       <textarea name="calc_window" class="calc_window" disabled></textarea>
+      <div class="buttons_block">
+        
+      </div>
     </form>
   </body>
 </html>
